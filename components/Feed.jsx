a29bf2b42ad1d802c94a8281e5a3150d965409ef
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-05-05 15:08:39 -0800

    form-complete

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,38 @@
-'use client'
+"use client";
 
-import { useState, useEffect} from "react"
-import PromptCard from './PromptCard'
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
+const PromptCardList = ({data, handleTagClick}) =>{
+  return(
+    <div className="mt-16 prompt_layout">
+        {data.map((post) => 
+          <PromptCard 
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        )}
+    </div>
+  )
+}
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {
+
+  };
+
+  useEffect(() =>{
+    const fetchPosts = async () =>{
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, [])
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -13,11 +42,16 @@ const Feed = () => {
           value={searchText}
           onChange={handleSearchChange}
           required
-        >
-        </input>
+          className="search_input peer"
+        ></input>
       </form>
-    </section>
-  )
-}
 
-export default Feed
+      <PromptCardList 
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
+  );
+};
+
+export default Feed;
